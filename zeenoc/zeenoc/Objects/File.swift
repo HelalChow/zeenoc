12ba12047a8109bb3d0289f1b96bd0a0f43756c5
hchowdhury3273 <hc2324@nyu.edu>
2020-12-09 22:51:30 -0500

    property object and setting up tableView

---
+++ zeenoc/zeenoc/Objects/File.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  zeenoc
+//
+//  Created by Helal Chowdhury on 12/9/20.
+//
+
+import Foundation
+
+struct Property {
+    let tenantName: String
+    let address: String
+    let deadline: String
+    let rent: String
+}
+
--- zeenoc/zeenoc/ViewControllers/LandlordVC.swift
+++ zeenoc/zeenoc/ViewControllers/LandlordVC.swift
@@ -9,21 +9,39 @@ import UIKit
 
 class LandlordVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var Properties : [
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+extension LandlordVC: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath) as! PropertyCell
+ 
     }
-    */
+}
 
+extension LandlordVC: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        self.tableView.register(UINib(nibName: "PropertyCell", bundle: nil), forCellReuseIdentifier: "PropertyCell")
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PropertyCell") as! PropertyCell
+        
+        let name = array[0].data[indexPath.row].transliteration
+        let translation = array[0].data[indexPath.row].en.meaning
+        let arabic = array[0].data[indexPath.row].name
+        cell.setCell(name: name, translation: translation, arabic: arabic, num: String(indexPath.row + 1) + ".")
+        
+        return cell
+    }
 }
