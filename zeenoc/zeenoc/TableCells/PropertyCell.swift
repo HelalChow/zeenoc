52b4272cdc0ed33dd660cb8dd4e01fb49f6332a1
hchowdhury3273 <hc2324@nyu.edu>
2020-12-09 22:19:17 -0500

    created cell

---
+++ zeenoc/zeenoc/TableCells/PropertyCell.swift
@@ -0,0 +1,23 @@
+//
+//  PropertyCell.swift
+//  zeenoc
+//
+//  Created by Helal Chowdhury on 12/9/20.
+//
+
+import UIKit
+
+class PropertyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
