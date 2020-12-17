f3d022befb6c507191f72eeac78b7cae9f07e428
hchowdhury3273 <hc2324@nyu.edu>
2020-12-17 18:08:55 -0500

    sign up done

---
+++ zeenoc/zeenoc/Helpers/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  zeenoc
+//
+//  Created by Helal Chowdhury on 12/17/20.
+//
+
+import Foundation
+
+struct Constants {
+    struct Storyboard {
+        static let landlordVC = "LandlordVC"
+        static let tenantVC = "TenantVC"
+    }
+}
--- zeenoc/zeenoc/ViewControllers/SignUpVC.swift
+++ zeenoc/zeenoc/ViewControllers/SignUpVC.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import FirebaseAuth
+import Firebase
 
 class SignUpVC: UIViewController {
 
@@ -16,7 +18,7 @@ class SignUpVC: UIViewController {
     @IBOutlet weak var accountSegmentedControl: UISegmentedControl!
     @IBOutlet weak var signUpButton: UIButton!
     @IBOutlet weak var errorLabel: UILabel!
-    
+    var accountType = "tenants"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,9 +36,81 @@ class SignUpVC: UIViewController {
         Utilities.styleFilledButton(signUpButton)
         
     }
+    @IBAction func accountChanged(_ sender: Any) {
+        switch accountSegmentedControl.selectedSegmentIndex {
+        case 0:
+            accountType = "tenants"
+        case 1:
+            accountType = "landlords"
+        default:
+            break
+        }
+    }
+    
+    func validateFields() -> String? {
+        // Check if all fields are filled in
+        if firstNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            lastNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            print("Please fill in all fields")
+        }
+        
+        //Check if password is valid
+        let cleanPassword = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        if Utilities.isPasswordValid(cleanPassword) == false {
+            print("Password must contain: 8 characters, a number, special character")
+        }
+        
+        return nil
+    }
     
     @IBAction func signUpTapped(_ sender: Any) {
+        
+        let error = validateFields()
+        
+        if error != nil {
+            showError(error!)
+        } else {
+            let firstName = firstNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let lastName = lastNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            
+            //Create User
+            Auth.auth().createUser(withEmail: email, password: password) { (result, err) in
+                if err != nil {
+                    self.showError("Error Creating User")
+                } else {
+                    let db = Firestore.firestore()
+                    db.collection(self.accountType).addDocument(data: [
+                        "firstName": firstName,
+                        "lastName": lastName,
+                        "uid": result!.user.uid
+                    ]) { (error) in
+                        if error != nil {
+                            self.showError("Error Saving User Data")
+                        }
+                    }
+                    //Transition to Home Screen
+                    self.transitionToHome()
+                }
+            }
+            
+            
+        }
+    }
+    
+    func showError(_ message: String) {
+        errorLabel.text = message
+        errorLabel.alpha = 1
     }
     
 
+    func transitionToHome() {
+        let homeViewController = storyboard?.instantiateViewController(identifier: Constants.Storyboard.landlordVC) as? LandlordVC
+        
+        view.window?.rootViewController = homeViewController
+        view.window?.makeKeyAndVisible()
+    }
 }
