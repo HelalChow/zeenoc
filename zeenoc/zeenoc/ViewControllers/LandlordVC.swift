6072bf4e8cf2e74b3f9479794b778f938d079203
hchowdhury3273 <hc2324@nyu.edu>
2020-12-16 20:47:38 -0500

    chart start

--- zeenoc/zeenoc/ViewControllers/LandlordVC.swift
+++ zeenoc/zeenoc/ViewControllers/LandlordVC.swift
@@ -7,15 +7,24 @@
 
 import UIKit
 import Firebase
+import Charts
+import TinyConstraints
 
 var properties = [Property]()
 
 class LandlordVC: UIViewController {
 
+    @IBOutlet weak var pieChart: PieChartView!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var onTimeStepper: UIStepper!
+    @IBOutlet weak var missedStepper: UIStepper!
+    @IBOutlet weak var lateStepper: UIStepper!
     
-    let house1 = Property(id: "12345", tenantName: "Amy White", address: "9419 Linden Blvd Ozone Park NY 11417", deadline: "12/15/2020", rent: "$2000")
-    let house2 = Property(id: "6789", tenantName: "Samantha Brown", address: "8218 9th Ave Elmont NY 11635", deadline: "12/19/2020", rent: "$1800")
+    var onTimeDataEntry = PieChartDataEntry(value: 0)
+    var missedDataEntry = PieChartDataEntry(value: 0)
+    var lateDataEntry = PieChartDataEntry(value: 0)
+    
+    var numberOfEntries = [PieChartDataEntry]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,13 +35,30 @@ class LandlordVC: UIViewController {
             }
         }
         firebaseCall(completion: anonymousFunction)
-        
-        print("test")
-        print(properties)
         tableView.delegate = self
         tableView.dataSource = self
         
+        pieChart.chartDescription?.text = ""
+        
+        onTimeDataEntry.value = onTimeStepper.value
+        missedDataEntry.value = missedStepper.value
+        lateDataEntry.value = lateStepper.value
     }
+    
+    @IBAction func changeOnTime(_ sender: Any) {
+    }
+    @IBAction func changeMissed(_ sender: Any) {
+    }
+    @IBAction func changeLate(_ sender: Any) {
+    }
+    
+    func updateChartData() {
+        
+    }
+    
+    
+    
+    
     func firebaseCall(completion:@escaping([Property])->()) {
         let uid = Auth.auth().currentUser?.uid
         let db = Firestore.firestore()
@@ -55,62 +81,6 @@ class LandlordVC: UIViewController {
             }
         }
     }
-    
-//    func firebaseCall(completion:@escaping([Property])->()) {
-//        let uid = Auth.auth().currentUser?.uid
-//        let db = Firestore.firestore()
-//        db.collection("landlords").document("qgzC3W8vCEIPt7hfcHsQ").collection("properties").order(by: "deadline", descending: false).addSnapshotListener { (snap, err) in
-//            if err != nil {
-//                return
-//            }
-//            if snap!.documentChanges.isEmpty {
-//                return
-//            }
-//            for property in snap!.documentChanges {
-//                if property.type == .added {
-//                    let id = property.document.documentID
-//                    let name = property.document.get("tenantName") as! String
-//                    let address = property.document.get("address") as! String
-//                    let deadline = property.document.get("deadline") as! String
-//                    let rent = property.document.get("rent") as! Int
-//
-//                    properties.append(Property(id: id, tenantName: name, address: address, deadline: "12/" + deadline + "/2020", rent: "$" + String(rent)))
-//                    print(properties)
-//                }
-//                if property.type == .modified {
-//                    //when modified
-//                    let id = property.document.documentID
-//                    let name = property.document.get("tenantName") as! String
-//                    let address = property.document.get("address") as! String
-//                    let deadline = property.document.get("deadline") as! String
-//                    let rent = property.document.get("rent") as! Int
-//                    for i in 0..<properties.count {
-//                        if properties[i].id == id {
-//                            properties[i].deadline = deadline
-//                            properties[i].rent = String(rent)
-//                        }
-//                    }
-//                }
-//                if property.type == .removed {
-//                    //when removed
-//                    let id = property.document.documentID
-//                    for i in 0..<properties.count {
-//                        if properties[i].id == id {
-//                            properties.remove(at: i)
-//                            if properties.isEmpty {
-//
-//                            }
-//                            return
-//                        }
-//                    }
-//                }
-//                DispatchQueue.main.async {
-//                    completion(properties)
-//                }
-//            }
-//        }
-//    }
-    
 }
 
 extension LandlordVC: UITableViewDelegate{
@@ -122,8 +92,6 @@ extension LandlordVC: UITableViewDelegate{
 
 extension LandlordVC: UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print("")
-        print(properties.count)
         return properties.count
     }
 
@@ -136,9 +104,8 @@ extension LandlordVC: UITableViewDataSource{
         let address = properties[indexPath.row].address
         let deadline = properties[indexPath.row].deadline
         let rent = properties[indexPath.row].rent
+        
         cell.setProperty(tenantName: name, address: address, rentAmount: rent, deadline: deadline)
-        print("")
-        print(cell)
         return cell
     }
 }
