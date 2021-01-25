e787b9a0041f364238f073f402f8fae3ca6601c2
hchowdhury3273 <hc2324@nyu.edu>
2021-01-25 18:16:45 -0500

    mycell class

--- zeenoc/zeenoc/TableCells/MyPropertyCell.swift
+++ zeenoc/zeenoc/TableCells/MyPropertyCell.swift
@@ -8,18 +8,22 @@
 import UIKit
 
 class MyPropertyCell: UITableViewCell {
-
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var addressLabel: UILabel!
+    
+    @IBOutlet weak var tenantLabel: UILabel!
+    @IBOutlet weak var roomLabel: UILabel!
+    @IBOutlet weak var bathLabel: UILabel!
+    @IBOutlet weak var squareFootLabel: UILabel!
     @IBOutlet weak var rentLabel: UILabel!
-    @IBOutlet weak var deadlineLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
     
     
-    func setProperty(tenantName: String, address: String, rentAmount: String, deadline: String){
-        nameLabel.text = tenantName
-        addressLabel.text = address
+    func setProperty(tenantName: String, address: String, rentAmount: String, room: String, bath: String, squareFoot: String){
+        tenantLabel.text = tenantName
+        roomLabel.text = room
+        bathLabel.text = bath
+        squareFootLabel.text = squareFoot
         rentLabel.text = rentAmount
-        deadlineLabel.text = deadline
+        addressLabel.text = address
     }
     
 }
