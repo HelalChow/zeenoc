a6db2ecbaecde62f3815e6c27e120a2aae615bc1
hchowdhury3273 <hc2324@nyu.edu>
2021-01-30 19:22:27 -0500

    tenant intro page layout

--- zeenoc/zeenoc/ViewControllers/TenantVC.swift
+++ zeenoc/zeenoc/ViewControllers/TenantVC.swift
@@ -10,9 +10,14 @@ import Firebase
 
 class TenantVC: UIViewController {
 
+    @IBOutlet weak var propertyIDTextField: UITextField!
+    @IBOutlet weak var sendRequestButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        Utilities.styleTextField(propertyIDTextField)
+        Utilities.styleFilledButton(sendRequestButton)
+        
     }
     @IBAction func logOutTapped(_ sender: Any) {
         do {
