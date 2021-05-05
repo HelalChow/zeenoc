19e6844a05b9f6d1d09e2c10702a25ecbe5d10c0
Rabiya Sharieff <rs5981@nyu.edu>
2021-05-05 14:34:21 -0400

    server updates

--- zeenoc/server.py
+++ zeenoc/server.py
@@ -69,9 +69,27 @@ def assign_tenant_to_property(data): # creating a property in the properties col
     #db.collection("properties").add(data)
     db.collection("users").document(data["uid"]).add({"propertyID": data["propertyID"]})
 
-@app.route('/property/<data>', methods = ["POST"])
-def approve_tenant_to_property(data):
-    
+@app.route('/user/<data>', methods = ["POST"])
+def approve_tenant_to_property(data): #
+
+@app.route('/user/<data>', methods = ["POST"])
+def remove_tenant_from_property(data): # removing a tenant from assigned property
+    db.collection("users").document(data["uid"]).collection("properties").delete()
+
+
+@app.route('/payments/<data>', methods = ["POST"])
+def add_payment(data): # assigning payments to payments collection 
+    db.collection("users").document(ta["uid"]).collection("payments").add(data)
+
+@app.route('/payments/<data>', methods = ["POST"])
+def remove_payment(data): # removing payment
+    db.collection("users").document(data["uid"]).collection("payments").delete()
+
+
+@app.route('/tenant_info/<data>', methods = ["POST"]) #?????
+def request_tenant_info(data): # removing payment
+     db.collection("users").document(uid!).collection("requests").add(data)
+   
 
 if __name__ == '__main__':
     app.run()
